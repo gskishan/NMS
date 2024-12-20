e8f2a2d6e6fd16f49ac0e67dba04f130b4791e69
Shreyas <shreyasnaik01@gmail.com>
2024-12-20 13:27:48 +0530

    |#Asset|asset_customisation_and_fields|

--- nms/nms/doctype/asset_maintenance_request/asset_maintenance_request.js
+++ nms/nms/doctype/asset_maintenance_request/asset_maintenance_request.js
@@ -2,19 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Asset Maintenance Request", {
-    refresh: function(frm) {
+    refresh: function (frm) {
         if (!frm.is_new() && frm.doc.docstatus == 1) {
-            frm.add_custom_button(__("Asset Maintenance log"), function() {
-           
+            frm.add_custom_button(__("Asset Maintenance log"), function () {
                 frappe.new_doc("Asset Maintenance Log", {}, doc => {
-                 
-                });
-            
+                    const issue_html = frm.doc.issue_in_the_asset || "";
+
+                    const tempDiv = document.createElement("div");
+                    tempDiv.innerHTML = issue_html;
+                    const plainText = tempDiv.innerText || tempDiv.textContent || "";
+
+                    doc.issue_in_the_asset = plainText;
                     
-             
+                    frappe.set_route("Form", "Asset Maintenance Log", doc.name);
+                });
             });
         }
     }
-	
 });
+
 
