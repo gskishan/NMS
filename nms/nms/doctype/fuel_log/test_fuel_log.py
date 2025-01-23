995dfe9070a73c97617cdb97e179e3069299713f
ahmad406 <ahmadsayyed66@gmail.com>
2025-01-23 13:58:39 +0000

    fule log

---
+++ nms/nms/doctype/fuel_log/test_fuel_log.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, kishan and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestFuelLog(unittest.TestCase):
+	pass
---
+++ nms/nms/doctype/service_provider/test_service_provider.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025, kishan and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestServiceprovider(unittest.TestCase):
+	pass
