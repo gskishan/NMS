2c5ca37476e2c4a3af0fb6a4b93cc7071d329ba7
ahmad406 <ahmadsayyed66@gmail.com>
2025-01-23 15:20:27 +0000

    fuel log jv

--- nms/nms/doctype/fuel_log/fuel_log.js
+++ nms/nms/doctype/fuel_log/fuel_log.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Fuel Log', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		if (frm.doc.status==1){
+			frm.add_custom_button(__('Journal Entry'), function () {
+				frappe.new_doc('Journal Entry',{},doc =>{
+					doc.custom_fuel_log_no = frm.doc.name
+					doc.custom_lead = frm.doc.lead
+					doc.custom_estimation = frm.doc.estimation
+					doc.custom_quotation = frm.doc.quotation
+					doc.custom_project = frm.doc.project
+					doc.custom_sales_order = frm.doc.sales_order
+					doc.custom_sales_invoice = frm.doc.sales_invoice
+					doc.custom_planning = frm.doc.planning
+					doc.custom_task = frm.doc.task
 
-	// }
+
+				});
+			});
+		}
+
+	}
 });
