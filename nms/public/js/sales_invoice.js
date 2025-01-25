d323bfcf9c5f6b9cf7ddcbd78f2970562f907afa
G S Kishan <95840962+gskishan@users.noreply.github.com>
2025-01-25 11:25:07 +0530

    Update sales_invoice.js

--- nms/public/js/sales_invoice.js
+++ nms/public/js/sales_invoice.js
@@ -1,1 +1,22 @@
-h
+// Copyright (c) 2025, kishan and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Invoice', {
+	refresh: function(frm) {
+		if (frm.doc.docstatus==1){
+			frm.add_custom_button(__('Daily Report'), function () {
+				frappe.new_doc('Daily Report',{},doc =>{
+					doc.sales_invoice = frm.doc.name
+					doc.custom_client= frm.doc.customer
+					doc.custom_contract_type = frm.doc.estimation
+					doc.custom_cost_center= frm.doc.quotation
+					doc.project = frm.doc.project
+				
+
+
+				});
+			});
+		}
+
+	}
+});
